f58db2b9f56672f07dedcfe72b5be775a9a0e31b
谭锦威 <374055608@qq.com>
2019-07-05 15:02:55 +0800

    Updates

---
+++ precache-manifest.974c66da29bc2cd6983ada8f23fffd53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0f3771638bb8812b953431c717a2ce",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "c6029890d1f7187ede7f",
+    "url": "/react-demo/static/css/main.d4fee0fe.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c6029890d1f7187ede7f",
+    "url": "/react-demo/static/js/main.06ab2d9d.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
